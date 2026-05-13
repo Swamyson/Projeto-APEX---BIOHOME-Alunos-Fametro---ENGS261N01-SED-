e151fc8742f6b90910663c3939871582cafa347b
Anthony <ae289013@icloud.com>
2026-05-13 13:39:35 -0300

    Funcionalidade nova: Suporte PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -25,6 +26,33 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    VitePWA({
+  registerType: 'autoUpdate',
+
+  manifest: {
+    name: 'Apex BioHome',
+    short_name: 'Apex',
+    description: 'Monitoramento ambiental inteligente',
+    theme_color: '#000000',
+    background_color: '#000000',
+    display: 'standalone',
+    orientation: 'portrait',
+
+    icons: [
+      {
+        src: '/logo192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+
+      {
+        src: '/logo512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+}),
   ],
   resolve: {
     alias: {
